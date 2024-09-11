704a82c6cca68285009013eb34bdda88861661a7
Haggai <haggaibb@gmail.com>
2024-09-11 10:02:44 +0300

    Work on Verification New Verification Screen;
    -Open Issues-
    - null error while loading Verification screen

--- lib/auth_screens.dart
+++ lib/auth_screens.dart
@@ -8,7 +8,7 @@ import 'package:get/get.dart';
 import 'package:password_strength_checker/password_strength_checker.dart';
 import 'package:ypo_connect/main.dart';
 import 'members_controller.dart';
-import 'theme.dart';
+import 'dart:async';
 import 'onboarding.dart';
 import 'home.dart';
 
@@ -210,18 +210,9 @@ class _EmailRegisterFormState extends State<EmailRegisterForm> {
           if (credentials.user != null) {
             await controller.onRegister(credentials.user!);
             controller.loading.value = false;
-            Get.to(EmailVerificationScreen(
-              actions: [
-                EmailVerifiedAction(() {
-                  controller.onVerify(credentials.user!);
-                  Get.to(OnBoardingPage(user: widget.authController.auth.currentUser!));
-                }),
-                AuthCancelledAction((context) {
-                  FirebaseUIAuth.signOut(context: context);
-                  Navigator.pushReplacementNamed(context, '/');
-                }),
-              ],
-            ));
+            print('credentials.user!=null =>');
+            print(credentials.user!=null);
+            Get.to(() => const EmailVerificationScreen());
           }
         } on FirebaseAuthException catch  (e) {
           setState(() {
@@ -381,29 +372,134 @@ class _EmailRegisterFormState extends State<EmailRegisterForm> {
   }
 }
 
-class VerificationPage extends StatelessWidget {
-  final User user;
-  //final controller = Get.put(MembersController());
-  VerificationPage({super.key, required this.user});
+// class VerificationPage extends StatelessWidget {
+//   final User user;
+//   //final controller = Get.put(MembersController());
+//   const VerificationPage({super.key, required this.user});
+//   @override
+//   Widget build(BuildContext context) {
+//     final controller = Get.put(MembersController());
+//     return EmailVerificationScreen(
+//       actions: [
+//         EmailVerifiedAction(() {
+//           Get.to(OnBoardingPage(user: user));
+//           controller.onVerify(user);
+//           runApp(Home(user: user));
+//           //Navigator.pushReplacementNamed(context, '/onboarding');
+//         }),
+//         AuthCancelledAction((context) {
+//           FirebaseUIAuth.signOut(context: context);
+//           Get.to(FrontGate());
+//         }),
+//       ],
+//     );
+//   }
+// }
+
+class EmailVerificationScreen extends StatefulWidget {
+  const EmailVerificationScreen({ super.key});
+
+  @override
+  State<EmailVerificationScreen> createState() =>
+      _EmailVerificationScreenState();
+}
+
+class _EmailVerificationScreenState extends State<EmailVerificationScreen> {
+  bool isEmailVerified = false;
+  Timer? timer;
+  @override
+  void initState() {
+    super.initState();
+    print('init Verification screen');
+    FirebaseAuth.instance.currentUser?.sendEmailVerification();
+    timer = Timer.periodic(const Duration(seconds: 3), (_) => checkEmailVerified());
+  }
+
+  checkEmailVerified() async {
+    await FirebaseAuth.instance.currentUser?.reload();
+    setState(() {
+      isEmailVerified = FirebaseAuth.instance.currentUser!.emailVerified;
+    });
+
+    if (isEmailVerified) {
+      Get.to(() => OnBoardingPage(user: FirebaseAuth.instance.currentUser));
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text("Email Successfully Verified")));
+
+      timer?.cancel();
+    }
+  }
+
+  @override
+  void dispose() {
+// TODO: implement dispose
+    timer?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final controller = Get.put(MembersController());
-    return EmailVerificationScreen(
-      actions: [
-        EmailVerifiedAction(() {
-          Get.to(OnBoardingPage(user: user));
-          controller.onVerify(user);
-          runApp(Home(user: user));
-          //Navigator.pushReplacementNamed(context, '/onboarding');
-        }),
-        AuthCancelledAction((context) {
-          FirebaseUIAuth.signOut(context: context);
-          Get.to(FrontGate());
-        }),
-      ],
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 35),
+              const SizedBox(height: 30),
+              const Center(
+                child: Text(
+                  'Check your \n Email',
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              const SizedBox(height: 8),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                child: Center(
+                  child: Text(
+                    'We have sent you a Email on ${FirebaseAuth.instance.currentUser?.email}',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              const Center(child: CircularProgressIndicator()),
+              const SizedBox(height: 8),
+              const Padding(
+                padding: EdgeInsets
+                    .symmetric(horizontal: 32.0),
+                child: Center(
+                  child: Text(
+                    'Verifying email....',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 57),
+              Padding(
+                padding: const EdgeInsets
+                    .symmetric(horizontal: 32.0),
+                child: ElevatedButton(
+                  child: const Text('Resend'),
+                  onPressed: () {
+                    try {
+                      FirebaseAuth.instance.currentUser
+                          ?.sendEmailVerification();
+                    } catch (e) {
+                      debugPrint('$e');
+                    }
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
+
 
 /*
 EmailVerificationScreen(
